b7ed7ed9af16cf4faf7ec0e0dfccb1c3e3d1bc33
Sufyan Shaikh <34384008+shxkari@users.noreply.github.com>
2019-11-05 17:34:03 -0800

    Add files via upload

---
+++ lab10/lab10_ml.py
@@ -0,0 +1,77 @@
+#combine training data
+f1 = 'X_train.txt'
+f2 =  'y_train.txt'
+       
+combine =[]
+
+with open(f2) as xh:
+  with open(f1) as yh:
+    with open("train.txt","w") as zh:
+      #Read first file
+      xlines = xh.readlines()
+      #Read second file
+      ylines = yh.readlines()
+      #Combine content of both lists
+      #combine = list(zip(ylines,xlines))
+      #Write to third file
+      for i in range(len(xlines)):
+        line = ylines[i].strip() + ' ' + xlines[i]
+        zh.write(line)
+
+#close files
+zh.close()
+xh.close()
+yh.close()
+
+
+#combine test data
+e1 = 'X_test.txt'
+e2 =  'y_test.txt'
+       
+combine =[]
+
+with open(e2) as xh:
+  with open(e1) as yh:
+    with open("train.txt","w") as zh:
+      #Read first file
+      xlines = xh.readlines()
+      #Read second file
+      ylines = yh.readlines()
+      #Combine content of both lists
+      #combine = list(zip(ylines,xlines))
+      #Write to third file
+      for i in range(len(xlines)):
+        line = ylines[i].strip() + ' ' + xlines[i]
+        zh.write(line)
+
+#close open files
+zh.close()
+xh.close()
+yh.close()
+
+
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
+from pyspark.mllib.util import MLUtils
+
+# Load and parse the data file into an RDD of LabeledPoint.
+testData = MLUtils.loadLibSVMFile(sc, 'path')
+trainingData = MLUtils.loadLibSVMFile(sc, 'path')
+
+
+
+# Split the data into training and test sets (30% held out for testing)
+(trainingData, testData) = data.randomSplit([0.7, 0.3])
+
+# Train a DecisionTree model.
+#  Empty categoricalFeaturesInfo indicates all features are continuous.
+model = DecisionTree.trainClassifier(trainingData, numClasses=12, categoricalFeaturesInfo={},
+                                     impurity='gini', maxDepth=5, maxBins=32)
+
+# Evaluate model on test instances and compute test error
+predictions = model.predict(testData.map(lambda x: x.features))
+labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+testErr = labelsAndPredictions.filter(
+    lambda lp: lp[0] != lp[1]).count() / float(testData.count())
+print('Test Error = ' + str(testErr))
+print('Learned classification tree model:')
+print(model.toDebugString())
